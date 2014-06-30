ef2b6ebd019e754725c2f5e4d9369be50b5610c2
Phillip Harris <phillip.harris@atlanticbt.com>
2014-06-30 11:17:20 -0400

    Initial Commit

---
+++ CollectionViewSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionViewSample
+//
+//  Created by Phillip Harris on 6/30/14.
+//  Copyright (c) 2014 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionViewSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionViewSample
+//
+//  Created by Phillip Harris on 6/30/14.
+//  Copyright (c) 2014 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
